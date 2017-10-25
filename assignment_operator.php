1592ea1db2131be5f4d67b1854176266adf443af
hamza12358 <sheikh-hamz@hotmail.com>
2017-10-26 03:53:11 +0500

    PHP

---
+++ assignment_operator.php
@@ -0,0 +1,28 @@
+<?php
+
+$number = 10;
+$number +=2;
+
+echo $number;
+
+?>
+<br>
+
+<?php
+
+$number = 10;
+$number -=2;
+
+echo $number;
+
+?>
+<br>
+
+<?php
+
+$text = 'Hello';
+$text .= ' world';
+
+echo $text;
+?>
+<br>
